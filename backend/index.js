2f5820cacc6d044244ff96a02478c63dde6ac6f4
faiyaz7070 <malickfaiyaz415@gmail.com>
2024-02-26 11:39:36 +0530

    again

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,29 @@
-const express=require("express");
-const connection=require("./db")
-const userRouter=require("./routes/user.route");
-const postRouter=require("./routes/post.route");
-const bodyParser = require('body-parser');
-require("dotenv").config()
-const cors=require("cors");
-const app=express();
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+const Connection = require("./config/db");
+const  userRouter  = require("./routes/user");
+const postRouter  = require("./routes/post");
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('<h1>Cointab SE-ASSIGNMENT</h1><button onclick="getAllUsers()">All Users</button>');
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to cointab assignment");
 });
 
-app.use("/", userRouter);
-app.use("/", postRouter);
-
-app.listen(process.env.port, async()=>{
-    try {
-        await connection;
-        console.log("Connected to DB");
-    } catch (error) {
-        console.log(error);
-    }
-    console.log(`Server is running on port ${process.env.port}`);
-})
+app.listen(4500, async () => {
+  try {
+    console.log(`Server is running on http://localhost 4500`);
+    console.log("DB is connected");
+    await Connection;
+  } catch (error) {
+    console.log(error.message);
+  }
+});
